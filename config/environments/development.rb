a829855fcd4c0aa5aa9a532af402cd1acc20f383
Matt Ripley <matt@engr2-1-197-131-dhcp.int.colorado.edu>
2012-04-04 17:50:43 -0600

    added devise integration. created use model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ FaceTagger::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #config mailer options for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
