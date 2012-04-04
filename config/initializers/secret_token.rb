9bc04477cb5eddedbff255b6ad193579587fd993
Matt Ripley <matt@engr2-1-197-131-dhcp.int.colorado.edu>
2012-04-04 16:22:23 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaceTagger::Application.config.secret_token = 'ee4c998c3fe31f4846c26c9909b66e04216aba9ccde8a6f28e0d12201f7ed801e8f0ae06b41198a41890d0534d4cd092ba60b2fc80a7dee9260d0be1e49b7864'
